0bb20e6537b348d84bdd79a3c79a61a39f83999a
Stephen Weil <stephen.weil@gmail.com>
2018-11-09 23:13:34 -0600

    Fix asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { 
+  config.action_mailer.default_url_options = {
     host: 'www.westhoochington.com',
     protocol: 'https',
   }
