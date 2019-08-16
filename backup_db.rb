9d3a79f800ed0ecd167fc5862f04717594a6f977
Stephen Weil <stephen.weil@gmail.com>
2019-08-15 19:27:14 -0500

    Add seasons to backup script

--- backup_db.rb
+++ backup_db.rb
@@ -13,7 +13,7 @@ HEADERS = [
   'Opponent Projected Points'
 ]
 
-FILE_PATH = "/home/sjweil/Documents/Coding/data_backups/westhoochington/20190727.csv"
+FILE_PATH = "/home/sjweil/Documents/Coding/data_backups/westhoochington/#{Date.today.strftime('%m%d%Y')}_games.csv"
 
 CSV.open(FILE_PATH, 'w', write_headers: true, headers: HEADERS) do |out_file|
   Game.includes(:user, :opponent).references(:user, :opponent).each do |game|
@@ -28,6 +28,22 @@ CSV.open(FILE_PATH, 'w', write_headers: true, headers: HEADERS) do |out_file|
       game.opponent_active_total,
       game.opponent_bench_total,
       game.opponent_projected_total
+    ]
+    out_file << csv_row
+  end
+end
+
+SEASON_FILE_PATH = "/home/sjweil/Documents/Coding/data_backups/westhoochington/#{Date.today.strftime('%m%d%Y')}_seasons.csv"
+
+SEASON_HEADERS = ['Searon Year', 'User', 'Playoff Rank', 'Regular Season Rank']
+
+CSV.open(SEASON_FILE_PATH, 'w', write_headers: true, headers: SEASON_HEADERS) do |out_file|
+  Season.includes(:user).references(:user).each do |season|
+    csv_row = [
+      season.season_year,
+      season.user.email,
+      season.playoff_rank,
+      season.regular_rank
     ]
     out_file << csv_row
   end
