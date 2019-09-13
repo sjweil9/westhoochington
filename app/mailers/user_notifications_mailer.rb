726fdfd69699ffc74e1c50cc85bd17fae2f67a9a
Stephen Weil <stephen.weil@gmail.com>
2019-09-12 22:38:51 -0500

    Initial set up and first couple implementations for random newsletter translations

--- app/mailers/user_notifications_mailer.rb
+++ app/mailers/user_notifications_mailer.rb
@@ -17,6 +17,7 @@ class UserNotificationsMailer < ApplicationMailer
     @narrowest = @games.sort_by { |a| a.margin.abs }.first
     @largest = @games.sort_by { |a| -a.margin.abs }.first
     @high_score = @games.detect { |game| game.weekly_high_score?(@games) }
+    set_random_messages!
     mail(to: emails, subject: "Weekly Westhoochington - #{year} ##{week}")
   end
 
@@ -35,5 +36,36 @@ class UserNotificationsMailer < ApplicationMailer
 
   def user_joins
     [:"games_#{@year}", :"opponent_games_#{@year}", nicknames: :votes]
+  end
+
+  def set_random_messages!
+    @high_score_message = random_high_score_message
+    @narrowest_cucking_message = random_narrow_cucking_message
+  end
+
+  def random_high_score_message
+    i18n_key = if @high_score.active_total > 150
+                 'newsletter.high_score.very_high'
+               elsif @high_score.active_total > 130
+                 'newsletter.high_score.high'
+               elsif @high_score.active_total < 120
+                 'newsletter.high_score.low'
+               else
+                 'newsletter.high_score.medium'
+               end
+    possible_messages = I18n.t(i18n_key).keys
+    I18n.t([i18n_key, possible_messages.sample].join('.'), nickname: @high_score.winner.random_nickname, score: @high_score.active_total)
+  end
+
+  def random_narrow_cucking_message
+    i18n_key = if @narrowest.margin.abs < 5
+                 'newsletter.narrowest.very_narrow'
+               elsif @narrowest.margin.abs < 10
+                 'newsletter.narrowest.medium'
+               else
+                 'newsletter.narrowest.not_narrow'
+               end
+    possible_messages = I18n.t(i18n_key).keys
+    I18n.t([i18n_key, possible_messages.sample].join('.'), winner: @narrowest.winner.random_nickname, loser: @narrowest.loser.random_nickname, margin: @narrowest.margin.abs)
   end
 end
