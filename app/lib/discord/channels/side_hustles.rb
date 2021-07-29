5e8b3f5b32f112deae3504b0a8060b96685b3f6f
Stephen Weil <stephen.weil@gmail.com>
2021-07-28 21:55:52 -0500

    Go back to real channel webhook

--- app/lib/discord/channels/side_hustles.rb
+++ app/lib/discord/channels/side_hustles.rb
@@ -2,8 +2,7 @@ module Discord
   module Channels
     class SideHustles < Base
       def webhook_url
-        #Rails.application.credentials.side_hustles_webhook.freeze
-        Rails.application.credentials.testing_webhook.freeze
+        Rails.application.credentials.side_hustles_webhook.freeze
       end
     end
   end
