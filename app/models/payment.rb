212d3e508fc41b9590f8a7d9dca42fc6230d04cd
Stephen Weil <stephen.weil@gmail.com>
2021-08-18 22:01:24 -0500

    Add payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,23 @@
 class Payment < ApplicationRecord
   belongs_to :user
+
+  PAYOUT_MAP = {
+    buy_in: 150,
+    weekly_payout: 20,
+    first_place: 900,
+    second_place: 360,
+    third_place: 150,
+    regular_season_winner: 150
+  }.with_indifferent_access
+
+  validate :validate_bet_type
+
+  private
+
+  def validate_bet_type
+    return if PAYOUT_MAP.keys.include?(type)
+
+    errors.add(:payment_type, "Not a valid type.")
+  end
 end
+
--- db/migrate/20210819025025_create_payments.rb
+++ db/migrate/20210819025025_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration[5.2]
   def change
     create_table :payments do |t|
       t.references :user, foreign_key: true
-      t.string :type
+      t.string :payment_type
       t.numeric :amount
       t.integer :season_year
       t.integer :week
