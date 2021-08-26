8bd7e87d0eba6001c0a3d27a380beb4e68c02c7f
Stephen Weil <stephen.weil@gmail.com>
2021-08-26 15:18:44 -0500

    Finish initial position command for stats bot

--- app/lib/discord/bots/commands/position.rb
+++ app/lib/discord/bots/commands/position.rb
@@ -7,9 +7,17 @@ module Discord
         end
 
         def execute(event, *args)
+          return invalid_position!(event, args[0]) unless VALID_POSITIONS.include?(args[0])
+          second_arg_is_year = args[1].to_i.to_s == args[1]
+          user_arg = second_arg_is_year ? nil : args[1]
+          year_arg = second_arg_is_year ? args[1] : args[2]
+          return invalid_user!(event, user_arg) unless !user_arg || valid_user?(user_arg)
+          return invalid_year!(event, year_arg) unless !year_arg || valid_year?(year_arg)
+
           case args.length
           when 1 then position_total(args[0], event)
-          when 2 then user_total(args[0], args[1], event)
+          when 2 then second_arg_is_year ? year_total(args[0], year_arg, event) : user_total(args[0], user_arg, event)
+          when 3 then user_year_total(args[0], args[1], args[2], event)
           end
 
           nil
@@ -20,59 +28,116 @@ module Discord
         VALID_POSITIONS = %w[QB RB WR TE DST K].freeze
 
         def min_args; 1; end
-        def max_args; 4; end
-        def channels; %w[testing].freeze end
+        def max_args; 3; end
+        def channels
+          Rails.env.production? ? %w[stat-requests].freeze : %w[testing].freeze
+        end
 
         def description
           "Retrieves stats from www.westhoochington.com.".freeze
         end
 
         def usage
-          "position [QB/RB/WR/TE/DST/K] [@user] [year] [week]".freeze
+          "position [QB/RB/WR/TE/DST/K] [@user/year] [year]".freeze
+        end
+
+        def user_year_total(position, discord_mention, year, event)
+          discord_id = discord_mention.gsub(/\D+/, '')
+          user_record = User.find_by(discord_id: discord_id)
+          results = ratio_for_user(user_record, position, year)
+          event << "#{user_record.random_nickname} scored #{results[:percentage]}% of their points in #{year} from the #{position} position (#{results[:position].round(2).to_s(:delimited)} / #{results[:total].round(2).to_s(:delimited)})."
+          position_points = PlayerGame.joins(:game).where(active: true, default_lineup_slot: position, games: { season_year: year }).all.sum(&:points)
+          total_points = PlayerGame.joins(:game).where(active: true, games: { season_year: year }).all.sum(&:points)
+          percentage = ((position_points / total_points) * 100).round(2)
+          event << "#{percentage}% of all points in #{year} (#{position_points.round(2).to_s(:delimited)} / #{total_points.round(2).to_s(:delimited)}) come from the #{position} position."
         end
 
         def user_total(position, user, event)
           discord_id = user.gsub(/\D+/, '')
           user_record = User.find_by(discord_id: discord_id)
-          if user_record
-            results = ratio_for_user(user_record, position)
-            event << "#{user_record.random_nickname} has scored #{results[:percentage]}% of their lifetime points from the #{position} position (#{results[:position].round(2).to_s(:delimited)} / #{results[:total].round(2).to_s(:delimited)})."
-          else
-            event << "Sorry, but I did not recognize #{user}."
+          results = ratio_for_user(user_record, position)
+          event << "#{user_record.random_nickname} has scored #{results[:percentage]}% of their lifetime points from the #{position} position (#{results[:position].round(2).to_s(:delimited)} / #{results[:total].round(2).to_s(:delimited)})."
+          position_points = PlayerGame.where(active: true, default_lineup_slot: position).all.sum(&:points)
+          total_points = PlayerGame.where(active: true).all.sum(&:points)
+          percentage = ((position_points / total_points) * 100).round(2)
+          event << "#{percentage}% of all points in League History (#{position_points.round(2).to_s(:delimited)} / #{total_points.round(2).to_s(:delimited)}) come from the #{position} position."
+        end
+
+        def year_total(position, year, event)
+          conditions = { active: true, games: { season_year: year } }
+          position_points = PlayerGame.joins(:game).where(conditions.merge(default_lineup_slot: position)).all.sum(&:points)
+          total_points = PlayerGame.joins(:game).where(conditions).all.sum(&:points)
+          percentage = ((position_points / total_points) * 100).round(2)
+          event << "A total of #{position_points.round(2).to_s(:delimited)} points were scored at the #{position} position in #{year}."
+          event << "This constitutes #{percentage}% of all #{total_points.round(2).to_s(:delimited)} points scored in #{year}."
+          event << "Here are all league members sorted by the proportion of their points from the #{position} position in #{year}:"
+          year_users = Game.where(season_year: year).all.map(&:user).uniq
+          results = year_users.reduce([]) do |memo, user|
+            results_for_user = ratio_for_user(user, position, year)
+            if results_for_user[:total].zero?
+              memo
+            else
+              memo + [results_for_user.merge(user: user.discord_mention)]
+            end
+          end.sort_by { |hash| -hash[:percentage] }
+          results.each_with_index do |result, index|
+            event << "#{index + 1}) #{result[:user]} -- #{result[:percentage]}% (#{result[:position].round(2).to_s(:delimited)} / #{result[:total].round(2).to_s(:delimited)})"
           end
         end
 
         def position_total(position, event)
-          if VALID_POSITIONS.include?(position)
-            position_points = PlayerGame.where(active: true, default_lineup_slot: position).all.sum(&:points)
-            all_points = PlayerGame.where(active: true).all.sum(&:points)
-            percentage = ((position_points / all_points) * 100).round(2)
-            event << "A total of #{position_points.round(2).to_s(:delimited)} points have been scored at the #{position} position in league history."
-            event << "This constitutes #{percentage}% of all #{all_points.round(2).to_s(:delimited)} points that have been scored in league history."
-            event << "Here are all active league members sorted by the proportion of their points from the #{position} position:"
-            active_users = User.where(active: true).all
-            results = active_users.reduce([]) do |memo, user|
-              results_for_user = ratio_for_user(user, position)
-              if results_for_user[:total].zero?
-                memo
-              else
-                memo + [results_for_user.merge(user: user.discord_mention)]
-              end
-            end.sort_by { |hash| -hash[:percentage] }
-            results.each_with_index do |result, index|
-              event << "#{index + 1}. #{result[:user]} -- #{result[:percentage]}% (#{result[:position].round(2).to_s(:delimited)} / #{result[:total].round(2).to_s(:delimited)})"
+          position_points = PlayerGame.where(active: true, default_lineup_slot: position).all.sum(&:points)
+          total_points = PlayerGame.where(active: true).all.sum(&:points)
+          percentage = ((position_points / total_points) * 100).round(2)
+          event << "A total of #{position_points.round(2).to_s(:delimited)} points have been scored at the #{position} position in league history."
+          event << "This constitutes #{percentage}% of all #{total_points.round(2).to_s(:delimited)} points that have been scored in league history."
+          event << "Here are all active league members sorted by the proportion of their points from the #{position} position:"
+          active_users = User.where(active: true).all
+          results = active_users.reduce([]) do |memo, user|
+            results_for_user = ratio_for_user(user, position)
+            if results_for_user[:total].zero?
+              memo
+            else
+              memo + [results_for_user.merge(user: user.discord_mention)]
             end
-          else
-            event << "Invalid position #{position}. Must be one of #{VALID_POSITIONS.join(', ')}."
+          end.sort_by { |hash| -hash[:percentage] }
+          results.each_with_index do |result, index|
+            event << "#{index + 1}) #{result[:user]} -- #{result[:percentage]}% (#{result[:position].round(2).to_s(:delimited)} / #{result[:total].round(2).to_s(:delimited)})"
           end
         end
 
         def ratio_for_user(user, position, year = nil, week = nil)
-          filter_conditions = { active: true, user_id: user.id, year: year, week: week }.compact
-          total_points = PlayerGame.joins(:game).where(filter_conditions).all.sum(&:points)
-          position_points = PlayerGame.joins(:game).where(filter_conditions.merge(default_lineup_slot: position)).all.sum(&:points)
+          filter_conditions = { active: true, user_id: user.id }.compact
+          game_conditions = { season_year: year, week: week }.compact
+          total_conditions = game_conditions.present? ? filter_conditions.merge(games: game_conditions) : filter_conditions
+          total_points = PlayerGame.joins(:game).where(total_conditions).all.sum(&:points)
+          position_points = PlayerGame.joins(:game).where(total_conditions.merge(default_lineup_slot: position)).all.sum(&:points)
           percentage = total_points.zero? ? 0.0 : ((position_points / total_points) * 100).round(2)
           { total: total_points, position: position_points, percentage: percentage }
+        end
+
+        def invalid_position!(event, position)
+          event << "Invalid position #{position}. Must be one of #{VALID_POSITIONS.join(', ')}."
+          nil
+        end
+
+        def invalid_user!(event, user)
+          event << "Sorry, user #{user} was not recognized."
+          nil
+        end
+
+        def invalid_year!(event, year)
+          event << "Year #{year} is invalid. Must be between 2015 and the present."
+          nil
+        end
+
+        def valid_user?(discord_mention)
+          discord_id = discord_mention.gsub(/\D+/, '')
+          User.find_by(discord_id: discord_id)&.discord_id.present?
+        end
+
+        def valid_year?(year)
+          year.to_i >= 2015 && year.to_i <= Date.today.year
         end
       end
     end
--- config/initializers/discord_bot.rb
+++ config/initializers/discord_bot.rb
@@ -1,6 +1,6 @@
 require "discordrb"
 
-bot = Discord::Bots::Stats.new(token: Rails.application.credentials.westhoochington_bot_token, prefix: "!", help_command: "statshelp")
+bot = Discord::Bots::Stats.new(token: Rails.application.credentials.westhoochington_bot_token, prefix: "!", help_command: :statshelp)
 
 bot.run(true)
 
