c4f48b20512a3265f83f3093df4611b8d9e1134e
Stephen Weil <stephen.weil@gmail.com>
2021-08-26 15:28:52 -0500

    Add comment

--- config/initializers/discord_bot.rb
+++ config/initializers/discord_bot.rb
@@ -1,5 +1,6 @@
 require "discordrb"
 
+# solution from: https://stackoverflow.com/questions/15538587/how-do-i-detect-in-rails-if-i-am-running-a-rake-command
 is_rake = (ENV['RACK_ENV'].blank? || ENV['RAILS_ENV'].blank? || !("#{ENV.inspect}" =~ /worker/i).blank?)
 
 unless is_rake
