feb01e56bf2c0ce5310b277d428408dd765c2ff8
Stephen Weil <stephen.weil@gmail.com>
2023-08-08 12:22:55 -0500

    wrap arel.sql

--- app/models/player_game.rb
+++ app/models/player_game.rb
@@ -3,6 +3,6 @@ class PlayerGame < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
-  scope :lineup_order, -> { order("array_position(ARRAY['QB', 'RB', 'WR', 'TE', 'FLEX', 'DST', 'K', 'BN']::varchar[], lineup_slot)") }
+  scope :lineup_order, -> { order(Arel.sql("array_position(ARRAY['QB', 'RB', 'WR', 'TE', 'FLEX', 'DST', 'K', 'BN']::varchar[], lineup_slot)")) }
   scope :active, -> { where(active: true) }
 end
