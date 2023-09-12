b6664d8aa19f6ce329fe2c36d398e64b71ccb3cd
Stephen Weil <stephen.weil@gmail.com>
2023-09-12 11:30:18 -0500

    Default to bot if no sleeper user mappipng

--- app/jobs/sleeper/update_best_ball_results_job.rb
+++ app/jobs/sleeper/update_best_ball_results_job.rb
@@ -5,7 +5,7 @@ module Sleeper
       parsed = JSON.parse(response.body)
       league = BestBallLeague.find_by(sleeper_id: league_id)
       parsed.each do |object|
-        user = User.find_by(sleeper_id: object["owner_id"])
+        user = User.find_by(sleeper_id: object["owner_id"]) || User.find_by(email: "bot@sleeper.com")
         league_user = BestBallLeagueUser.find_or_create_by(best_ball_league: league, user: user)
         total_points = [object.dig("settings", "fpts"), object.dig("settings", "fpts_decimal")].join(".").to_f
         league_user.update(roster_id: object["roster_id"], total_points: total_points)
