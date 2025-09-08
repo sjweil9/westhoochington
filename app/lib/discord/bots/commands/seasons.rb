51b05c36df3d047941362077ce56e4dc40ae50a5
Stephen Weil <stephen.weil@gmail.com>
2025-09-08 16:11:04 -0500

    Add log statement

--- app/lib/discord/bots/commands/seasons.rb
+++ app/lib/discord/bots/commands/seasons.rb
@@ -22,6 +22,10 @@ module Discord
           user = find_user(arg3) unless is_year
           return invalid_user!(event, arg3) unless is_year || user
 
+          p "**********************************"
+          p "Processing command with: #{args}"
+          p "**********************************"
+
           stat_key = KEY_MAPPING[stat]
 
           if args.length == 2
@@ -108,7 +112,7 @@ module Discord
         end
 
         def usage
-          "seasons [highest/lowest] [pointsfor/pointsagainst/mojo/mojoagainst] [@user/year]".freeze
+          "seasons [highest/lowest] [pointsfor/pointsagainst/opponentmojo] [@user/year]".freeze
         end
 
         def invalid_direction!(event, direction)
@@ -119,64 +123,6 @@ module Discord
         def valid_user?(discord_mention)
           discord_id = discord_mention.gsub(/\D+/, '')
           User.find_by(discord_id: discord_id)&.discord_id.present?
-        end
-
-        GAME_INCLUDES = %i[player_games user opponent].freeze
-
-        def historical_total(direction, direction_field, event, tryhard)
-          games = Game.without_two_week_playoffs.includes(*GAME_INCLUDES).references(*GAME_INCLUDES).order(direction_field => direction)
-          games = games.where.not(id: PlayerGame.where(active: true, projected_points: 0).select(:game_id)) if tryhard
-          games = games.where("season_year >= 2018") if direction_field == :projected_total
-          msg = "Here are the top 10 #{direction == :asc ? 'lowest' : 'highest'} #{direction_field == :projected_total ? 'projected ' : ''}1-week scores in league history"
-          msg << " (excluding games where a player projected to score zero was started)" if tryhard
-          event << "#{msg}:"
-          games.first(10).each_with_index do |game, index|
-            event << "#{index + 1}) #{game.send(direction_field)}#{direction_field == :projected_total ? " (#{game.active_total} final)" : ""} -- #{game.user.random_nickname} vs #{game.opponent.random_nickname} -- #{game.season_year}, Week #{game.week}"
-          end
-        end
-
-        def year_total(direction, direction_field, year, event, tryhard)
-          if RANGE_REGEX.match?(year)
-            start, finish = year.split("-")
-            year = (start..finish)
-          end
-          games = Game.without_two_week_playoffs.includes(*GAME_INCLUDES).references(*GAME_INCLUDES).where(season_year: year).order(direction_field => direction)
-          games = games.where.not(id: PlayerGame.where(active: true, projected_points: 0).select(:game_id)) if tryhard
-          games = games.where("season_year >= 2018") if direction_field == :projected_total
-          msg = "Here are the top 10 #{direction == :asc ? 'lowest' : 'highest'} #{direction_field == :projected_total ? 'projected ' : ''}1-week scores from #{year}"
-          msg << " (excluding games where a player projected to score zero was started)" if tryhard
-          event << "#{msg}:"
-          games.first(10).each_with_index do |game, index|
-            event << "#{index + 1}) #{game.send(direction_field)}#{direction_field == :projected_total ? " (#{game.active_total} final)" : ""} -- #{game.user.random_nickname} vs #{game.opponent.random_nickname} -- Week #{game.week}"
-          end
-        end
-
-        def user_total(direction, direction_field, user, event, tryhard)
-          games = Game.without_two_week_playoffs.includes(*GAME_INCLUDES).references(*GAME_INCLUDES).where(user_id: user.id).order(direction_field => direction)
-          games = games.where.not(id: PlayerGame.where(active: true, projected_points: 0).select(:game_id)) if tryhard
-          games = games.where("season_year >= 2018") if direction_field == :projected_total
-          msg = "Here are the top 10 #{direction == :asc ? 'lowest' : 'highest'} #{direction_field == :projected_total ? 'projected ' : ''}1-week scores by #{user.random_nickname}"
-          msg << " (excluding games where a player projected to score zero was started)" if tryhard
-          event << "#{msg}:"
-          games.first(10).each_with_index do |game, index|
-            event << "#{index + 1}) #{game.send(direction_field)}#{direction_field == :projected_total ? " (#{game.active_total} final)" : ""} -- #{game.user.random_nickname} vs #{game.opponent.random_nickname} -- #{game.season_year}, Week #{game.week}"
-          end
-        end
-
-        def user_year_total(direction, direction_field, user, year, event, tryhard)
-          if RANGE_REGEX.match?(year)
-            start, finish = year.split("-")
-            year = (start..finish)
-          end
-          games = Game.without_two_week_playoffs.includes(*GAME_INCLUDES).references(*GAME_INCLUDES).where(user_id: user.id, season_year: year).order(direction_field => direction)
-          games = games.where.not(id: PlayerGame.where(active: true, projected_points: 0).select(:game_id)) if tryhard
-          games = games.where("season_year >= 2018") if direction_field == :projected_total
-          msg = "Here are the top 5 #{direction == :asc ? 'lowest' : 'highest'} #{direction_field == :projected_total ? 'projected ' : ''}1-week scores by #{user.random_nickname} in #{year}:"
-          msg << " (excluding games where a player projected to score zero was started)" if tryhard
-          event << "#{msg}:"
-          games.first(5).each_with_index do |game, index|
-            event << "#{index + 1}) #{game.send(direction_field)}#{direction_field == :projected_total ? " (#{game.active_total} final)" : ""} -- #{game.user.random_nickname} vs #{game.opponent.random_nickname} -- #{game.season_year}, Week #{game.week}"
-          end
         end
       end
     end
