754d0aa676216b9cbd55817adce4d5254bf8efa1
Stephen Weil <stephen.weil@gmail.com>
2025-09-08 23:48:16 -0500

    Add 2025 email mapping

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -127,6 +127,20 @@ class ApplicationJob < ActiveJob::Base
       '11': 'sccrrckstr@gmail.com',
       '12': 'michael.i.zack@gmail.com',
       '13': 'austinlayton6@gmail.com'
+    },
+    '2025': {
+      '1': 'ovaisinamullah@gmail.com',
+      '2': 'mikelacy3@gmail.com',
+      '4': 'tonypelli@gmail.com',
+      '5': 'goblue101@gmail.com',
+      '6': 'pkaushish@gmail.com',
+      '7': 'adamkos101@gmail.com',
+      '8': 'stephen.weil@gmail.com',
+      '9': 'captrf@gmail.com',
+      '10': 'seidmangar@gmail.com',
+      '11': 'sccrrckstr@gmail.com',
+      '12': 'michael.i.zack@gmail.com',
+      '13': 'austinlayton6@gmail.com'
     }
   }.with_indifferent_access
 
